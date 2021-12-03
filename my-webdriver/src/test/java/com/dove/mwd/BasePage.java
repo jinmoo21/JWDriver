822f1e4e78e7a0da3514db22c48608af67f0a278
Jinmoo Han <jinmoo21@naver.com>
2021-12-03 16:32:26 +0900

    Update BasePage.java

--- my-webdriver/src/test/java/com/dove/mwd/BasePage.java
+++ my-webdriver/src/test/java/com/dove/mwd/BasePage.java
@@ -4,12 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.LoadableComponent;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public abstract class BasePage<T> extends LoadableComponent<BasePage<T>> {
 	protected final WebDriver driver;
-	protected final Logger logger = LoggerFactory.getLogger(getClass());
 
 	public BasePage(WebDriver driver) {
 		this.driver = driver;

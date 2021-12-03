063eaf6ce91c5c271df1a263718b08a618da7d23
Jinmoo Han <jinmoo21@naver.com>
2021-12-03 16:30:12 +0900

    Update NotDefinedException.java

--- my-webdriver/src/main/java/com/dove/mwd/NotDefinedException.java
+++ my-webdriver/src/main/java/com/dove/mwd/NotDefinedException.java
@@ -1,20 +1,12 @@
 package com.dove.mwd;
 
-import java.util.Optional;
-
-import com.gurock.testrail.APIClient;
-
-import lombok.Getter;
-
-public class TestRailManager {
-	@Getter
-	private APIClient client;
-
-	public TestRailManager() {
-		Optional.ofNullable(System.getProperty("testrail.url")).ifPresent(url -> {
-			client = new APIClient(url);
-			client.setUser(System.getProperty("testrail.id"));
-			client.setPassword(System.getProperty("testrail.pw"));
-		});
+@SuppressWarnings("serial")
+public class NotDefinedException extends RuntimeException {
+	NotDefinedException() {
+		super();
+	}
+	
+	NotDefinedException(String msg) {
+		super(msg);
 	}
 }
